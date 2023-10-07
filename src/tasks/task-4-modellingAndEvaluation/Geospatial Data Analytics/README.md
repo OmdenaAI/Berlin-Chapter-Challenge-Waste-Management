abde9c525318acb821a3ac549a5dfe56e8725651
Kartik Arya <82070742+ripper-18@users.noreply.github.com>
2023-10-07 12:40:27 +0530

    Dashboard in deployment

---
+++ src/tasks/task-4-modellingAndEvaluation/Geospatial Data Analytics/README.md
@@ -0,0 +1,19 @@
+Download shapefiles for Germany's Road Network from [here](https://mapcruzin.com/free-germany-arcgis-maps-shapefiles.htm)
+
+Drive link for germany_highwaysandmajorroads[here](https://drive.google.com/drive/folders/10q6jFNqJKQo9u-Mjaik9yfC2awVdqMZy?usp=sharing)
+
+# Route Optimisation Modelling
+
+# Data files - combined files. 
+
+Contains combiend file of various waste centres for all states of germany. 
+- Complete_geospatialdata_withoutfactors.csv file is the main file for our modelling. It contains id , longitude , latitude , state , station for each of the centres for each state in Germany. 
+- without factors means that the file contains no information regarding the categories related to recycling centres. 
+- rest of the files were used to create the above file using the code in Kavita-work.ipynb
+
+# Model.ipynb
+
+- This file contains code for fetching the routes and direction matrix using ORS API and optimising it. 
+- route_data.geojson file is the output file from the model.ipynb
+- route_map.html is the visualisation of the route_data.geojson file using folium. 
+
--- src/tasks/task-5-deployment/pages/4_Machine Learning Model.py
+++ src/tasks/task-5-deployment/pages/4_Machine Learning Model.py
@@ -35,7 +35,7 @@ with tab1:
 
     @st.cache_data
     def load_data():
-        df = pd.read_csv('data\Processed_DatasetsAmount-of Waste-Generated-By-State 32121-0003.csv')
+        df = pd.read_csv('data/Processed_DatasetsAmount-of Waste-Generated-By-State 32121-0003.csv')
         return df
 
     def states():
@@ -45,7 +45,7 @@ with tab1:
 
     @st.cache_resource
     def load_model():
-        model = pickle.load(open('models\wm-model.pkl','rb'))
+        model = pickle.load(open('models/wm-model.pkl','rb'))
         return model
 
     def show_search_query():
@@ -84,7 +84,7 @@ with tab2:
 
     @st.cache_data
     def load_data():
-        df = pd.read_csv('data\Processed_DatasetsAmount-of Waste-Generated-By-State 32121-0003.csv')
+        df = pd.read_csv('data/Processed_DatasetsAmount-of Waste-Generated-By-State 32121-0003.csv')
         return df
     
     @st.cache_data
@@ -95,7 +95,7 @@ with tab2:
 
     @st.cache_resource
     def load_model():
-        model = pickle.load(open('models\wm-model.pkl','rb'))
+        model = pickle.load(open('models/wm-model.pkl','rb'))
         return model
 
 
