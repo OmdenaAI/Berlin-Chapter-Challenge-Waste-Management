e1653a752783b43b2dff255ff367ab233e269af9
KaviRana <rkavita1203@gmail.com>
2023-10-07 12:42:17 +0530

    working on web app

--- src/tasks/task-5-deployment/pages/2_Dashboards.py
+++ src/tasks/task-5-deployment/pages/2_Dashboards.py
@@ -1,13 +1,6 @@
 import streamlit as st
 import webbrowser
-import sys
-from pathlib import Path
 
-dashboard_1 = Path("E:\\Berlin-Chapter-Challenge-Waste-Management\\src\\tasks\\task-3-EDA\\Dashboards\\Final Dashboards\\Mirambika-hazardous_waste_dashboard")  # Replace with the actual path to your modules
-sys.path.append(str(dashboard_1))
-
-# Now you can import your modules
-from dashboard_hazardous_waste  import display_dashboard_1
 
 
 st.title("Dashboards")
@@ -21,8 +14,8 @@ st.write(
 tab1, tab2, tab3 = st.tabs(['Dashboard 1', 'Dashboard 2', 'Dashboard 3'])
 
 with tab1:
-    st.header("Dashboard 1")
-    display_dashboard_1()
+    st.header("Dashboard 1") 
+    
 
 with tab2:
     st.header("Dashboard 2")
@@ -31,3 +24,4 @@ with tab2:
 with tab3:
     st.header("Dashboard 3")
     # Add code to display content from Dashboard 3 here.
+
